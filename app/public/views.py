709ead17aa961dcec311d80dfbf7c15915939e10
Andros Fenollosa <andros@fenollosa.email>
2024-05-19 16:34:16 +0200

    Update

--- app/public/views.py
+++ app/public/views.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.shortcuts import render
 from .actions.home import get_context as get_home_context
-from liveview.utils import get_html
 
 async def home(request):
-    return render(request, "layouts/base.html", await get_home_context())
+    return render(request, settings.TEMPLATE_BASE, await get_home_context())
